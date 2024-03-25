dd8501df3cfeed667787ea15b3aa76ff328f54b1
IlariaGuarnieri <ilaria.guarnieri@gmail.com>
2024-03-25 11:01:47 +0100

    [add] aggiunta prezzo

--- src/components/partials/ProductCard.vue
+++ src/components/partials/ProductCard.vue
@@ -7,14 +7,25 @@
         myCards
       }
     },
-    methods:{
-      getImagePath(image){
-        return new URL(`../../../public/img/${image}`, import.meta.url).href;
-      }
-    },
+    // non lo uso perche le img sono in public e non in src (collegato a riga 34)
+    // methods:{
+    //   getImagePath(image){
+    //     return new URL(`../../../public/img/${image}`, import.meta.url).href;
+    //   }
+    // },
     computed:{
         cardCycle() {
         return (this.myCards.products);
+      },
+      discountPrice(){
+        let discountExist = false;
+        let discount;
+        let priceDiscount;
+        this.CardObj.badges.forEach(b=>{
+          if(b.type =='discount'){
+
+          }
+        })
       }
     }
   }
@@ -29,13 +40,19 @@
   class="container-card">
   
     <div>
-      <img :src="getImagePath(item.frontImage)" alt="immagine prodotto">
+      <img class="product-levis" :src="`/img/${item.frontImage}`" alt="immagine prodotto">
+      <!-- // non lo uso perche le img sono in public e non in src (collegato a riga 10) -->
+      <!-- <img :src="getImagePath(item.frontImage)" alt="immagine prodotto"> -->
+      <img class="product-levis" id="levis-none" :src="`/img/${item.backImage}`" alt="immagine prodotto">
     </div>
     <div class="text relaxed">
       <span class="brand">{{ item.brand }} </span>
+      <span class="hearts">&hearts;</span>
+        <div class="sconto">{{item.badges.value}}</div>
+        <div class="sostenibilità"> Sostenibilità</div>
       <h1 class="clothes-name">{{ item.name }}</h1>
-      <span class="solded-price">{{ item.soldedPrice }} &euro;</span>
-      <span class="original-price">{{ item.originalPrice }} &euro;</span>
+      <span class="solded-price">{{ item.price }} &euro;</span>
+      <span class="original-price">{{ item.originalPrice }} </span>
     </div>
   </section>
 </template>
@@ -55,9 +72,51 @@
     margin: 10px;
     position: relative;
     color: black;
-    img{
-      object-fit: cover;
+    .img-fronte:hover{
+      display:block;
     }
   }
+
+  .product-levis{
+  width: 300px;
+  height: 500px;
+  margin: 5px;
+}
+  #levis-none{
+  display: none;
+}
+
+.product:hover #levis-none{
+  display: block;
+}
+
+.product:hover .product-levis{
+  display: none;
+}
+
+.sconto{
+  position: absolute;
+  top: 450px;
+  left:40px ;
+  background-color: red;
+  color: white;
+}
+
+.sostenibilità{
+  position: absolute;
+  top: 450px;
+  left: 80px;
+  background-color: green;
+  color: white;
+}
+
+.hearts{
+  font-size: 30px;
+  position: absolute;
+  left: 310px;
+  top: 20px;
+  background-color: white;
+  width: 40px;
+}
 
 </style>
